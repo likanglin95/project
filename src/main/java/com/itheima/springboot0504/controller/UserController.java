a82c50a3b08ff9b5a9a87e10f94f10de57646663
likanglin <1111369@greatwall.com.cn>
2020-02-26 09:10:19 +0800

    version03

--- src/main/java/com/itheima/springboot0504/controller/UserController.java
+++ src/main/java/com/itheima/springboot0504/controller/UserController.java
@@ -9,21 +9,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Map;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
-    @RequestMapping("login")
-    public String login(String username, Map<String,Object> map){
+
+    @RequestMapping("/login")
+    public String login(String username, Map<String, Object> map, String password) {
         User user = this.userService.selectByName(username);
-        if (user==null){
-            map.put("err",1);
+        if (user == null) {
+            map.put("err", "1");
+            System.out.println("集合为空");
             return "login";
-        }
-        else{
-            map.put("err",0);
-            return "redirect:/test/show";
+        } else {
+            System.out.println(user.getPassword());
+            if (user.getPassword().equals(password)) {
+                map.put("err", "0");
+                return "redirect:/test/show";
+            } else {
+                map.put("err", "2");
+                return "login";
+            }
         }
     }
+    @RequestMapping("/index")
+    public String index(Map<String,Object> map){
+        map.put("err","");
+        return "login";
+    }
+
 
 }
